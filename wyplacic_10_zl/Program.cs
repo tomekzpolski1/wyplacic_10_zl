917d473d63fab2116e9955217b5f71b4dce80194
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-15 09:31:24 +0100

    Add project files.

---
+++ wyplacic_10_zl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace wyplacic_10_zl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            for (int z1 = 0; z1<=10 ; z1++)
+            {
+                for (int z2 = 0; z2 <=5; z2++)
+                {
+                    for (int z5 = 0; z5 < 2; z5++)
+                    {
+                        if (z1 * 1 + z2 * 2 + z5 * 5 == 10)
+                        {
+                            Console.WriteLine("zlotowka = {0} dwa zlote = {1} piec zlote = {2}", z1, z2, z5);
+                        }
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
